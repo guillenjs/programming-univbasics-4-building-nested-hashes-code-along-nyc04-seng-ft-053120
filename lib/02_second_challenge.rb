29f3bf53d05bb30a2c15b5c6aa04d93f50876b11
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-09 21:50:56 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
-  epic_tragedy =
-  {
-    :montague =>{
+  epic_tragedy = {
+    :montague => {
       patriarch: {},
       matriarch: {},
       hero: {},
       hero_friends: {}
       },
-
     :capulet => {
       patriarch: {},
       matriarch: {},
-      heroine: {},
+      hero: {},
       heroine_friends: {}
-    }
-  }
+      #your key/value pairs here!
+      }}
+
+
 end
