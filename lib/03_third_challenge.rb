b7c6e3f9de12d443157289d7613bf1cfcd753d96
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-09 21:59:58 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -2,16 +2,17 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name:"",
-        age:
+        name: "Lord Montague",
+        age: "53"
         },
       :matriarch => {
-        name:"",
-        age:
+        name:"Lady Montague",
+        age: "54"
         },
       :hero => {
-        name:"",
-        age:
+        name:"Romeo",
+        age: "15",
+        status: "alive"
         },
       :hero_friends => []
    },
